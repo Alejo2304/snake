8456f4516a2b1cea6c44f708a92c959fe4fee729
Alejo2304 <ojeda.alejandro.2304@gmail.com>
2023-07-28 15:04:33 -0500

    Setup the initial config for pygame, added an fps limit as well fill the color of the background

--- snake.py
+++ snake.py
@@ -1,19 +1,27 @@
-import sys, pygame, time
+import sys, pygame
 
 #main varibles
 size = width, height = 800, 600
+fps =  60
 
-#initialize the screen
-pygame.init
-
-window = pygame.display.set_mode(size)  #creates the window. 
-pygame.display.set_caption('Snake Game') #change caption of the window
-screen = pygame.display.get_surface()
-
+#Main Functions
 def exit(): #this function is used to quit the loop while True.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit(0)
 
+#initialize pygame.
+pygame.init
+screen = pygame.display.set_mode(size)  #creates the window. 
+pygame.display.set_caption('Snake Game') #change caption of the window
+clock = pygame.time.Clock()
+
+
+
 while True:
-    exit()
+    #draw all our elements
+    exit() #check for the event pygame.QUIT and exit the execution.
+    
+    screen.fill(pygame.Color('green')) #fill the background with green color.
+    clock.tick(fps) #limitates the speed of the drawing. 
+    pygame.display.update()
